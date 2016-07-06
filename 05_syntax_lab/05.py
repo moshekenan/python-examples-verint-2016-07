44af8f38c8bb53575d078905088eebb77745b22e
moshekenan <moshe.kenan@verint.com>
2016-07-06 16:55:15 +0300

    Solution for: sol-python-verint.syntax-lab/05.py

--- 05_syntax_lab/05.py
+++ 05_syntax_lab/05.py
@@ -1,5 +1,15 @@
 """
-Write a program that randomizes integers in a loop
-until it finds a number that is divisable by: 7, 13 and 15
+Syntax excersize #5
 """
+sum_of_digits = 0
+rand = 0
 
+from random import randint
+
+while True:
+    rand = randint(1, 1000000)
+    #print rand
+
+    if rand % 7 == 0 and rand % 13 == 0 and rand % 15 == 0: break 
+   
+print "The random number: {} is divisable by 7 and by 13 and by 15".format(rand)
