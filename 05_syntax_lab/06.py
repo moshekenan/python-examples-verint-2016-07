d82ac235addba568cbbed54e9ec5eafe3f707f8f
moshekenan <moshe.kenan@verint.com>
2016-07-06 17:22:10 +0300

    Solution for: sol-python-verint.syntax-lab/06.py

--- 05_syntax_lab/06.py
+++ 05_syntax_lab/06.py
@@ -1,9 +1,26 @@
 """
-Write a program that randomizes 2 numbers
-and calculates their least common multiplier,
-that is the smallest number that is divisable
-by both.
-For example if the numbers were 4 and 6,
-program should print 12
+Syntax excersize #6
 """
+sum_of_digits = 0
+rand1 = 0
+rand2 = 0
+common = 0
 
+from random import randint
+
+rand1 = randint(1, 10)
+rand2 = randint(1, 10)
+print rand1
+print rand2
+
+bigger = rand1
+
+if rand2 > rand1:
+    bigger = rand2
+
+for n in range(bigger,100):
+    if n % rand1 == 0 and n % rand2 == 0:
+        common = n
+        break 
+   
+print "The lowest common multiplication of {} and {} is {}".format(rand1, rand2, common)
