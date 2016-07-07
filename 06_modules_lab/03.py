58a5fd971e40320745d786dcbf53d7442b584d98
moshekenan <moshe.kenan@verint.com>
2016-07-07 15:27:05 +0300

    Solution for: sol-python-verint.modules-lab/03.py

--- 06_modules_lab/03.py
+++ 06_modules_lab/03.py
@@ -1,11 +1,37 @@
-""" Write a program that searches current working directory
-for files larger than 1MB. Every time you find such a file print
-its name to the user.
+import os
+import sys
 
-- When the program finds a large file. It should ask the user
-  a message asking if she wants to delete it, and delete the
-  file if requested
+if len(sys.argv) > 2:
 
-- Take threshold and path as command line arguments
-"""
+    min_size_to_delete = 0
 
+    search_dir = sys.argv[1]
+    try:
+        min_size_to_delete = int(sys.argv[2])
+
+        print "searching for files in '%s' bigger than %d bytes" % (search_dir,min_size_to_delete)
+
+        if os.path.exists(search_dir):
+            for root, dirs, files in os.walk(search_dir):
+                for name in files:
+                    #print "found file: %s/%s" % (root, name)
+
+                    full_path = "{}\{}".format(root,name)
+                    file_size = os.path.getsize(full_path)
+                    if file_size > min_size_to_delete:
+                        print "found file %s with size: %d bytes" % (full_path,file_size)
+                        print "if you want to delete this file enter 'd'"
+                        del_file = raw_input()
+                        if del_file == 'd':
+                            os.remove(full_path)
+        else:
+            print "The directory %s does not exists" % search_dir
+
+
+    except ValueError:
+        print "second argument <min_size_to_delete> must be numeric"
+
+
+else:
+    program_name = sys.argv[0]
+    print "Please pass 2 arguments, Usage: %s <search_dir> <min_size_to_delete>" % program_name
