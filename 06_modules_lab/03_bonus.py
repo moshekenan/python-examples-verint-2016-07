f47bc7a0317c78af29b05d0753889faebe6700b3
moshekenan <moshe.kenan@verint.com>
2016-07-07 17:34:04 +0300

    Solution for: sol-python-verint.modules-lab/03_bonus.py

--- 06_modules_lab/03_bonus.py
+++ 06_modules_lab/03_bonus.py
@@ -1,13 +1,35 @@
-""" Write a program that searches current working directory
-for files larger than 1MB. Every time you find such a file print
-its name to the user.
+import os
+import argparse
 
-- When the program finds a large file. It should ask the user
-  a message asking if she wants to delete it, and delete the
-  file if requested
+# Assign description to the help doc
+parser = argparse.ArgumentParser(description='Script search directory to find and delete large files.')
 
-- Take threshold and path as command line arguments
+# Add arguments
+parser.add_argument('-d', '--directory', type=str, help='Directory path', required=True)
+parser.add_argument('-s', '--size', type=int, help='minimum size to delete', required=True)
 
-Bonus: Use argparse module to parse command line arguments
-"""
+# Array for all arguments passed to script
+args = parser.parse_args()
+
+# Assign args to variables
+search_dir = args.directory
+min_size_to_delete = args.size
+
+print "searching for files in '%s' bigger than %d bytes" % (search_dir, min_size_to_delete)
+
+if os.path.exists(search_dir):
+    for root, dirs, files in os.walk(search_dir):
+        for name in files:
+            #print "found file: %s/%s" % (root, name)
+
+            full_path = "{}\{}".format(root,name)
+            file_size = os.path.getsize(full_path)
+            if file_size > min_size_to_delete:
+                print "found file %s with size: %d bytes" % (full_path,file_size)
+                print "if you want to delete this file enter 'd'"
+                del_file = raw_input()
+                if del_file == 'd':
+                    os.remove(full_path)
+else:
+    print "The directory %s does not exists" % search_dir
 
