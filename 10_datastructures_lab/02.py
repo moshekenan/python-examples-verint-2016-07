60f1ff4e06c8d8f89c105053378f618abb0d53df
moshekenan <moshe.kenan@verint.com>
2016-07-10 19:46:29 +0300

    Solution for: sol-python-verint.datastructures-lab/02.py

--- 10_datastructures_lab/02.py
+++ 10_datastructures_lab/02.py
@@ -1,5 +1,22 @@
 """
-Write a program that prints only unique values
-from sys.argv (without program name)
+excersize #2
+check above average grades in list
 """
+from random import randint
 
+sum_of_grades = 0
+
+grades = range(20)
+
+for n in grades:
+   grades[n] = randint(40, 100)
+   sum_of_grades += grades[n]
+
+average = sum_of_grades/20
+print grades
+print "average: {}".format(average)
+
+print "baove average grades are:"
+for grade in grades:
+    if grade > average:
+        print "{}".format(grade)
