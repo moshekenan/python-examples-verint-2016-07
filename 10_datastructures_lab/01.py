0f0bc0e37144a85cb89b2327d54cc56d6d0b00b7
moshekenan <moshe.kenan@verint.com>
2016-07-10 19:46:25 +0300

    Solution for: sol-python-verint.datastructures-lab/01.py

--- 10_datastructures_lab/01.py
+++ 10_datastructures_lab/01.py
@@ -1,10 +1,23 @@
+# -*- coding: utf-8 -*-
 """
-Write a program that takes two strings
-from the user and checks if they represent
-a valid user name.
-Valid users and passwords:
-    apple => red
-    lettuce => green
-    lemon => yellow
-    orange => orange
+excersize #1
+Check username and password
 """
+
+#list of valid usernames and passwords
+valid_accounts = {'apple' : 'red', 'lettuce' : 'green', 'lemon' : 'yellow', 'orange' : 'orange' }
+
+#user input
+user_name = raw_input("Please enter username: ")
+password = raw_input("Please enter password: ")
+
+#return_str = "access denied"
+return_str = u"אין כניסה"
+
+#check validity
+for valid in valid_accounts:
+    if user_name == valid and password == valid_accounts[valid]:
+        #return_str = "Welcome"
+        return_str = u"ברוכים הבאים"        
+
+print return_str        
