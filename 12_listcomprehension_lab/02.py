c52a813af326b4975937ba4d1ebafd1eed22bbb9
moshekenan <moshe.kenan@verint.com>
2016-07-11 16:58:33 +0300

    Solution for: sol-python-verint.list-comprehension-lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -1,6 +1,11 @@
 """
-Write a python program that takes two words
-as sys.argv and prints only the letters
-common to both
+excersize #1
+Print list of all possible 3 lowercase English letters sequence
 """
+
+letters = range(ord('a'),ord('z')+1)
+
+c = [chr(x)+chr(y)+chr(z) for x in letters for y in letters for z in letters]
+
+print c
 
