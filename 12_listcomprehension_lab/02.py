b585601a35b6f4782e2615516f411954835de1c1
moshekenan <moshe.kenan@verint.com>
2016-07-11 18:12:40 +0300

    Solution for: sol-python-verint.list-comprehension-lab/02.py

--- 12_listcomprehension_lab/02.py
+++ 12_listcomprehension_lab/02.py
@@ -3,7 +3,8 @@ excersize #1
 Print list of all possible 3 lowercase English letters sequence
 """
 
-letters = range(ord('a'),ord('z')+1)
+#letters = range(ord('a'),ord('z')+1)
+letters = [x for x in range(256) if x >= ord('a') and x <= ord('z')]
 
 c = [chr(x)+chr(y)+chr(z) for x in letters for y in letters for z in letters]
 
