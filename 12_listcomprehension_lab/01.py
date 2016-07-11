b48bba0d8476a50b1be43e5f6b102daefd46542b
moshekenan <moshe.kenan@verint.com>
2016-07-11 18:12:37 +0300

    Solution for: sol-python-verint.list-comprehension-lab/01.py

--- 12_listcomprehension_lab/01.py
+++ 12_listcomprehension_lab/01.py
@@ -3,6 +3,7 @@ excersize #1
 Print list of all English lowercase letters
 """
 
-x = range(ord('a'),ord('z')+1)
-for i in x: print chr(i)
+#x = range(ord('a'),ord('z')+1)
+lowercase_letters = [x for x in range(256) if x >= ord('a') and x <= ord('z')]
+for i in lowercase_letters: print chr(i)
 
