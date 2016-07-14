4204e6540a34a581312560ee087be26ce105deb4
moshekenan <moshe.kenan@verint.com>
2016-07-14 18:22:41 +0300

    Solution for: sol-python-verint.functions-lab/02.py

--- 14_functions_lab/02.py
+++ 14_functions_lab/02.py
@@ -1,7 +1,9 @@
-"""
-Write a function that takes two arguments: 
-    A string
-    And a number
-If wrong types were passed in, raise an exception
-"""
+def check_types(number, string):
+    if type(number) != int: raise Exception("Parameter 'number' must be an Integer")
+    if type(string) != str: raise Exception("Parameter 'string' must be a String")
 
+    print "parameters are ok: {} {}".format(number, string)
+
+check_types(3, 'fdfgdf')
+#check_types('dd', 'gjjf')
+check_types(3, 67)
