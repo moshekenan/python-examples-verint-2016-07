0377b75f3385a23978f373818a31e335081902b6
moshekenan <moshe.kenan@verint.com>
2016-07-14 18:34:05 +0300

    Solution for: sol-python-verint.functions-lab/03.py

--- 14_functions_lab/03.py
+++ 14_functions_lab/03.py
@@ -1,5 +1,16 @@
-"""
-Write a function that calculates the sum
-of the 10th digit from all arguments passed to it
-"""
+'''
+functios no3
+'''
+def sum_deca_digits(*nums):
+    sum = 0
+    for num in nums:
+        if type(num) != int: raise Exception("All Parameters must be Integers")    
+        div = num/10
+        mod = div % 10
+        sum += mod
+
+    return sum
+
+print sum_deca_digits(140,220,1120)        
+
 
