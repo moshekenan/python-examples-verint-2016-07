45db94f27a6eea5cdd566811ff4ffc4bf1da3cb7
moshekenan <moshe.kenan@verint.com>
2016-07-18 11:54:55 +0300

    Solution for: sol-python-verint.regexp-lab/01.py

--- 18_regexp_lab/01.py
+++ 18_regexp_lab/01.py
@@ -1,12 +1,31 @@
 """
-Write a program that reads data
-from property files.
-Each line in the file can either be:
-    An empty line
-    A comment line (Start with #)
-    A property line (of the form key = value)
-
-Write a program that takes a property file name and key
-as command line arguments and prints the requested value
+Regular expressions no 1
 """
+import sys
+import os
+import argparse
+import re
+
+# Assign description to the help doc
+parser = argparse.ArgumentParser(description='Find value of a key.')
+
+# Add arguments
+parser.add_argument('-s', '--sourcefile', type=str, help='source definitions file', required=True)
+parser.add_argument('-k', '--key', type=str, help='key for search', required=True)
+
+# Array for all arguments passed to script
+args = parser.parse_args()
+
+# Assign args to variables
+src_file = args.sourcefile
+key_to_find = args.key
+
+with open(src_file, "r") as fin:
+    for line in fin:
+        m = re.search(r'(\w+)\s*=\s*(\w+)', line)
+        if m is not None:
+            if m.group(1) == key_to_find:
+                print m.group(2)
+                break
+
 
