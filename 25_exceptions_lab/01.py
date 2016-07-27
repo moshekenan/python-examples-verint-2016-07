5d4a8f62beebd454213eb603c8a2147d58a0898c
moshekenan <moshe.kenan@verint.com>
2016-07-27 20:47:43 +0300

    Solution for: sol-python-verint.exceptions-lab/01.py

--- 25_exceptions_lab/01.py
+++ 25_exceptions_lab/01.py
@@ -1,7 +1,17 @@
-"""
-Write a python program that takes numbers in a loop
-and for each number prints its square root
-If value is negative or not a number show 
-a warning and keep reading values
-"""
+import math
+def sqrt(n):
+    if n < 0:
+        raise ValueError("Expected parameter >= 0, Got: %d" % n)
+    return math.sqrt(n)
 
+while True:
+    print "Please enter number to calculate square root:"
+    number = int(raw_input())
+    try:
+        print sqrt(number)
+    except ValueError as e:
+        print "Failed to calculate sqrt of {}. Error was: {}".format(number, e.message)
+    except Exception as e:
+        print "Something went wrong. Sorry", e
+    finally:
+        print "Thanks for using Python Sqrt Calculator"
